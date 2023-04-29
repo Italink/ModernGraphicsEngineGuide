bded65051e8c7599da0789f5a1e282c9c170ffbb
italink <657959053@qq.com>
2023-04-29 13:05:36 +0800

    [feature] 增加3D线条绘制

--- Source/3-GraphicsTechnology/00-Spline/Source/main.cpp
+++ Source/3-GraphicsTechnology/00-Spline/Source/main.cpp
@@ -1,13 +1,42 @@
 #include <QApplication>
 #include "QRenderWidget.h"
+#include "Render/QFrameGraph.h"
 #include "Render/Pass/QBasePassForward.h"
+#include "Render/Component/QSplineRenderComponent.h"
 
 int main(int argc, char** argv) {
 	QApplication app(argc, argv);
 	QRhiWindow::InitParams initParams;
+	initParams.backend = QRhi::Implementation::Vulkan;
 	QRenderWidget widget(initParams);
-	widget.setupCamera();
-	widget.resize({ 800,600 });
+
+	widget.setupCamera()
+		->setPosition(QVector3D(0,0, 200));
+
+	QList<QSplinePoint> points;
+
+	int numOfPoint = 10;
+	int scaleFactor = 10;
+
+	for (int i = 0; i < numOfPoint; i++) {
+		QSplinePoint point;
+		point.mPoint.setX((i - numOfPoint/2 ) * scaleFactor);
+		point.mPoint.setY(i % 2 ? scaleFactor : -scaleFactor);
+		points << point;
+	}
+
+	widget.setFrameGraph(
+		QFrameGraph::Begin()
+		.addPass(
+			QBasePassForward::Create("BasePass")
+			.addComponent(
+				QSplineRenderComponent::Create("Spline")
+			.setPoints(points)
+			)
+		)
+		.end("BasePass", QBasePassForward::BaseColor)
+	);
+	widget.resize({ 1200,800 });
 	widget.show();
 	return app.exec();
 }
