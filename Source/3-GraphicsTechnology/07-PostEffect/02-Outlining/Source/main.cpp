b5e809a64200835e3751aa813136bcc4cb15113b
italink <657959053@qq.com>
2023-09-03 20:02:50 +0800

    [feature] 增加操作提示，实现描边示例

--- Source/3-GraphicsTechnology/07-PostEffect/02-Outlining/Source/main.cpp
+++ Source/3-GraphicsTechnology/07-PostEffect/02-Outlining/Source/main.cpp
@@ -1,34 +1,36 @@
 #include "QEngineApplication.h"
 #include "QRenderWidget.h"
 #include "Render/QFrameGraph.h"
-#include "Render/Pass/PBR/QPbrBasePassDeferred.h"
+#include "Render/Pass/QBasePassForward.h"
 #include "Render/Component/QStaticMeshRenderComponent.h"
-#include "Render/Pass/QMotionBlurRenderPass.h"
+#include "Render/Pass/QOutliningRenderPass.h"
+#include "Render/Pass/PBR/QPbrBasePassDeferred.h"
 
 int main(int argc, char** argv) {
 	QEngineApplication app(argc, argv);
 	QRhiWindow::InitParams initParams;
 	initParams.backend = QRhi::Implementation::Vulkan;
 	QRenderWidget widget(initParams);
-	widget.setupCamera()
-		->setPosition(QVector3D(0, 0, 25));
+	auto camera = widget.setupCamera();
+	camera->setPosition(QVector3D(20, 15, 12));
+	camera->setRotation(QVector3D(-30, 145, 0));
+
 	widget.setFrameGraph(
 		QFrameGraph::Begin()
 		.addPass(
 			QPbrBasePassDeferred::Create("BasePass")
 			.addComponent(
 				QStaticMeshRenderComponent::Create("StaticMesh")
-				.setStaticMesh(QStaticMesh::CreateFromFile(RESOURCE_DIR"/Model/mandalorian/scene.gltf"))
-				.setTranslate(QVector3D(0, -5, 0))
+				.setStaticMesh(QStaticMesh::CreateFromFile(RESOURCE_DIR"/Model/mandalorian_ship/scene.gltf"))
 				.setRotation(QVector3D(-90, 0, 0))
 			)
 		)
 		.addPass(
-			QMotionBlurRenderPass::Create("MotionBlur")
+			QOutliningRenderPass::Create("Outlining")
 			.setTextureIn_BaseColor("BasePass", QPbrBasePassDeferred::Out::BaseColor)
 			.setTextureIn_Position("BasePass", QPbrBasePassDeferred::Out::Position)
 		)
-		.end("MotionBlur", QMotionBlurRenderPass::Result)
+		.end("Outlining", QOutliningRenderPass::Result)
 	);
 
 	widget.showMaximized();
