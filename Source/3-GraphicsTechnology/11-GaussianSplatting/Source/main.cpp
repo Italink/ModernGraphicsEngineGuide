6eefc1032d14e294edc6ea96f0c1ccc63d309833
italink <657959053@qq.com>
2025-04-10 19:02:31 +0800

    [commit] 加入3DGS渲染

---
+++ Source/3-GraphicsTechnology/11-GaussianSplatting/Source/main.cpp
@@ -0,0 +1,44 @@
+#include "QEngineApplication.h"
+#include "QRenderWidget.h"
+#include "QtConcurrent/qtconcurrentrun.h"
+#include "Render/Component/QGaussianSplattingPointCloudRenderComponent.h"
+#include "Render/PassBuilder/QOutputPassBuilder.h"
+#include "Render/RenderGraph/PassBuilder/QMeshPassBuilder.h"
+
+class MyRenderer : public IRenderer {
+private:
+	QGaussianSplattingPointCloudRenderComponent m3DGSComp;
+	QSharedPointer<QGaussianSplattingPointCloud> GaussianSplattingPointCloudAsyncLoader;
+	QSharedPointer<QMeshPassBuilder> mMeshPass{ new QMeshPassBuilder };
+public:
+	MyRenderer()
+		: IRenderer({ QRhi::OpenGLES2 })
+	{
+		GaussianSplattingPointCloudAsyncLoader = QGaussianSplattingPointCloud::CreateFromFile("Resources/Model/ProjectTitan.ply");
+		m3DGSComp.setGaussianSplattingPointCloud(GaussianSplattingPointCloudAsyncLoader);
+
+		addComponent(&m3DGSComp);
+
+		setCurrentObject(&m3DGSComp);
+
+		getCamera()->setPosition(QVector3D(20, 15, 12));
+		getCamera()->setRotation(QVector3D(-30, 145, 0));
+	}
+protected:
+	void setupGraph(QRenderGraphBuilder& graphBuilder) override {
+		QMeshPassBuilder::Output meshOut
+			= graphBuilder.addPassBuilder("MeshPass", mMeshPass);
+
+		QOutputPassBuilder::Output cout
+			= graphBuilder.addPassBuilder<QOutputPassBuilder>("OutputPass")
+			.setInitialTexture(meshOut.BaseColor);
+	}
+};
+
+int main(int argc, char** argv) {
+	qputenv("QSG_INFO", "1");
+	QEngineApplication app(argc, argv);
+	QRenderWidget widget(new MyRenderer());
+	widget.showMaximized();
+	return app.exec();
+}
